7ff8565eed15e076e412db27607ee3fd6ac73ec7
Anatoliy Yakovlev <89045764700@mail.ru>
2020-05-16 23:21:05 +0700

    change home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { useSetToast } from '../../hooks'
 import {
 	MainSlider,
 	HomeNews,
@@ -10,28 +9,8 @@ import {
 import './Home.scss'
 
 const Home = () => {
-	const { setToast } = useSetToast()
-
-	const addToast = message => {
-		setToast(message)
-	}
-
 	return (
 		<>
-			<button
-				onClick={() =>
-					addToast({
-						data: {
-							title: 'error',
-							message: 'This is a success toast component',
-							type: 'success',
-						},
-						duration: 3000,
-					})
-				}
-			>
-				Добавить
-			</button>
 			<MainSlider />
 			<HomeNews />
 			<HitsSlider />
